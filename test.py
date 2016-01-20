c8255fac46cf8a96abe88a36dabe45bd706f2d43
Ted Kaemming <ted@kaemming.com>
2016-01-19 17:16:53 -0800

    Add temporary test script.

---
+++ test.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+
+from sentry.runner import configure
+configure()
+
+from datetime import timedelta
+from pprint import pprint
+
+from django.utils import timezone
+
+from sentry.app import tsdb
+
+
+tsdb.record_frequency_multi((
+    (tsdb.models.projects_by_organization, {
+        1: {
+            "foo": 1,
+            "bar": 2,
+            "baz": 3,
+        },
+    }),
+))
+
+pprint(
+    tsdb.get_most_frequent(
+        tsdb.models.projects_by_organization,
+        (1, 2, 3),
+        timezone.now(),
+    )
+)
+
+pprint(
+    tsdb.get_most_frequent(
+        tsdb.models.projects_by_organization,
+        (1, 2, 3),
+        timezone.now() - timedelta(hours=3),
+    )
+)
+
+pprint(
+    tsdb.get_frequency_series(
+        tsdb.models.projects_by_organization,
+        {
+            1: ("foo", "bar", "baz", "other"),
+            2: ("foo",),
+        },
+        timezone.now()
+    )
+)
+
+pprint(
+    tsdb.get_frequency_series(
+        tsdb.models.projects_by_organization,
+        {
+            1: ("foo", "bar", "baz", "other"),
+            2: ("foo",),
+        },
+        timezone.now() - timedelta(hours=3)
+    )
+)
+
+pprint(
+    tsdb.get_frequency_totals(
+        tsdb.models.projects_by_organization,
+        {
+            1: ("foo", "bar", "baz", "other"),
+            2: ("foo",),
+        },
+        timezone.now()
+    )
+)
+
+pprint(
+    tsdb.get_frequency_totals(
+        tsdb.models.projects_by_organization,
+        {
+            1: ("foo", "bar", "baz", "other"),
+            2: ("foo",),
+        },
+        timezone.now() - timedelta(hours=3)
+    )
+)
